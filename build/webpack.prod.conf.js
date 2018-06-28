6763ff61f022d5b4fb24746c0e63277c94590c42
冰原 <16657115156@163.com>
2018-06-28 16:23:51 +0800

    子模块更新了

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -64,6 +64,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: config.build.index,
       template: 'index.html',
       inject: true,
+      favicon: path.resolve('icon/favicon.ico'),
       minify: {
         removeComments: true,
         collapseWhitespace: true,
